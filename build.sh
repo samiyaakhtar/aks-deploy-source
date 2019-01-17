8f0d981322942c6e945429371663879bb97d110a
Samiya Akhtar <saakhta@microsoft.com>
2019-01-17 14:05:00 -0800

    Update script to install fab first

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
+
+echo "Downloading Fabrikate..."
+wget "https://github.com/Microsoft/fabrikate/releases/download/0.1.2/fab-v0.1.2-linux-amd64.zip"
+unzip fab-v0.1.2-linux-amd64.zip -d fab
+export PATH=$PATH:/home/vsts/work/1/s/fab
+
+echo "Running Fabrikate..."
 fab install
 fab generate prod
