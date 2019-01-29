da535371f76649f5456c4894851fb4fc42d368bd
Samiya Akhtar <saakhta@microsoft.com>
2019-01-29 13:40:49 -0800

    Merging my changes with yvonne's

--- generate.sh
+++ generate.sh
@@ -5,15 +5,16 @@ if [ -z "$VERSION" ]
 then
     VERSIONS=$(curl -s https://api.github.com/repos/Microsoft/fabrikate/tags)
     LATEST_RELEASE=$(echo $VERSIONS | grep "name" | head -1)
-    LATEST_VERSION=`echo "$LATEST_RELEASE" | cut -d'"' -f 4`
+    VERSION_TO_DOWNLOAD=`echo "$LATEST_RELEASE" | cut -d'"' -f 4`
 else
     echo "Fabrikate Version: $VERSION"
+    VERSION_TO_DOWNLOAD=$VERSION
 fi
 
 echo "Downloading Fabrikate..."
-echo "Latest Fabrikate Version: $LATEST_VERSION"
-wget "https://github.com/Microsoft/fabrikate/releases/download/$LATEST_VERSION/fab-v$LATEST_VERSION-linux-amd64.zip"
-unzip fab-v$LATEST_VERSION-linux-amd64.zip -d fab
+echo "Latest Fabrikate Version: $VERSION_TO_DOWNLOAD"
+wget "https://github.com/Microsoft/fabrikate/releases/download/$VERSION_TO_DOWNLOAD/fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip"
+unzip fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip -d fab
 export PATH=$PATH:/home/vsts/work/1/s/fab
 fab install
 
