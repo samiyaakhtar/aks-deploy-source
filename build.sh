f5578e08aafaaf375ddd627e19fdad03053429fa
Samiya Akhtar <saakhta@microsoft.com>
2019-03-06 12:56:55 -0800

    rm -rf

--- build.sh
+++ build.sh
@@ -145,8 +145,8 @@ function git_commit() {
     echo "GIT STATUS"
     git status
     echo "GIT REMOVE"
-    rm -r */
-    git rm */
+    rm -rf */
+    git rm -rf */
     echo "COPY YAML FILES TO REPO DIRECTORY..."
     cp -r $HOME/generated/* .
     echo "GIT ADD"
