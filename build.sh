5c0e760f59dfb86c7d2f79830372771921ab4b50
Samiya Akhtar <saakhta@microsoft.com>
2019-04-09 14:23:28 -0700

    testing requested for

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ function verify_access_token() {
     fi
 }
 function verify_repo() {
-    echo "CHECKING MANIFEST REPO URL"
-    if [[ -z "$MANIFEST_REPO" ]]; then
-        echo 'MANIFEST REPO URL not specified in variable $MANIFEST_REPO'
+    echo "CHECKING HLD/MANIFEST REPO URL"
+    if [[ -z "$REPO" ]]; then
+        echo 'HLD/MANIFEST REPO URL not specified in variable $REPO'
         exit 1
     fi
 }
@@ -23,17 +23,8 @@ function init() {
 
 # Initialize Helm
 function helm_init() {
-    echo "HELM INIT"
+    echo "RUN HELM INIT"
     helm init
-    echo "HELM ADD INCUBATOR"
-    if [ -z "$HELM_CHART_REPO" ] || [ -z "$HELM_CHART_REPO_URL" ];
-    then
-        echo "Using DEFAULT helm repo..."
-        helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
-    else
-        echo "Using DEFINED helm repo..."
-        helm repo add $HELM_CHART_REPO $HELM_CHART_REPO_URL
-    fi
 }
 
 # Obtain version for Fabrikate
@@ -92,45 +83,45 @@ function install_fab() {
 function fab_generate() {
     # For backwards compatibility, support pipelines that have not set this variable
     echo "CHECKING FABRIKATE ENVIRONMENTS"
-    if [ -z "$FAB_ENVS" ]; then 
-        echo "FAB_ENVS is not set" 
+    if [ -z "$FAB_ENVS" ]; then
+        echo "FAB_ENVS is not set"
         echo "FAB GENERATE prod"
-        fab generate prod --no-validation
-    else 
-        echo "FAB_ENVS is set to $FAB_ENVS" 
+        fab generate prod
+    else
+        echo "FAB_ENVS is set to $FAB_ENVS"
         IFS=',' read -ra ENV <<< "$FAB_ENVS"
         for i in "${ENV[@]}"; do
             echo "FAB GENERATE $i"
-            fab generate $i --no-validation
+            fab generate $i
         done
     fi
 
     echo "FAB GENERATE COMPLETED"
-    
+
     set +e
 
     # If generated folder is empty, quit
-    # In the case that all components are removed from the source hld, 
+    # In the case that all components are removed from the source hld,
     # generated folder should still not be empty
     if find "$HOME/generated" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
         echo "Manifest files have been generated."
     else
         echo "Manifest files could not be generated, quitting..."
         exit 1
-    fi  
+    fi
 }
 
 # Authenticate with Git
 function git_connect() {
     cd $HOME
     # Remove http(s):// protocol from URL so we can insert PA token
-    repo_url=$MANIFEST_REPO
+    repo_url=$REPO
     repo_url="${repo_url#http://}"
     repo_url="${repo_url#https://}"
     echo "GIT CLONE: https://automated:$ACCESS_TOKEN_SECRET@$repo_url"
 
     git clone https://automated:$ACCESS_TOKEN_SECRET@$repo_url
-    repo_url=$MANIFEST_REPO
+    repo_url=$REPO
     repo=${repo_url##*/}
 
     # Extract repo name from url
@@ -140,8 +131,11 @@ function git_connect() {
 
 # Git commit
 function git_commit() {
-    echo "GIT CHECKOUT"
-    git checkout master
+    echo "GIT CHECKOUT $BRANCH_NAME"
+    if ! git checkout $BRANCH_NAME ; then
+        git checkout -b $BRANCH_NAME
+    fi
+
     echo "GIT STATUS"
     git status
     echo "GIT REMOVE"
@@ -152,7 +146,7 @@ function git_commit() {
     echo "GIT ADD"
     git add -A
 
-    #Set git identity 
+    #Set git identity
     git config user.email "admin@azuredevops.com"
     git config user.name "Automated Account $(Build.RequestedFor)"
 
@@ -164,14 +158,14 @@ function git_commit() {
         echo "NOTHING TO COMMIT"
     fi
 
-    echo "GIT PULL" 
-    git pull
+    echo "GIT PULL origin $BRANCH_NAME"
+    git pull origin $BRANCH_NAME
 }
 
 # Perform a Git push
-function git_push() {  
+function git_push() {
     # Remove http(s):// protocol from URL so we can insert PA token
-    repo_url=$MANIFEST_REPO
+    repo_url=$REPO
     repo_url="${repo_url#http://}"
     repo_url="${repo_url#https://}"
 
@@ -183,7 +177,7 @@ function git_push() {
 }
 
 function unit_test() {
-    echo "Sourcing for unit test..."
+    echo "Sourcing build.sh ..."
 }
 
 function verify_pull_request() {
